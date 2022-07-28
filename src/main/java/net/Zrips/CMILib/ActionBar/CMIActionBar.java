1f069cf71591e0eaccf4d0af6e478be52e9e5d19
Zrips <Zripas@gmail.com>
2022-07-28 11:01:19 +0300

    1.19.1
    Raw and action bar message fix for new version

--- src/main/java/net/Zrips/CMILib/ActionBar/CMIActionBar.java
+++ src/main/java/net/Zrips/CMILib/ActionBar/CMIActionBar.java
@@ -35,205 +35,198 @@ public class CMIActionBar {
     private static Constructor<?> constructor;
 
     static {
-	if (Version.isCurrentEqualOrHigher(Version.v1_19_R1)) {
-	    try {
-		packetType = net.minecraft.network.protocol.game.ClientboundSystemChatPacket.class;
-		nmsIChatBaseComponent = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
-		nmsChatSerializer = Class.forName("net.minecraft.network.chat.IChatBaseComponent$ChatSerializer");
-		playerConnection = Class.forName("net.minecraft.server.level.EntityPlayer").getField("b");
-		sendPacket = Class.forName("net.minecraft.server.network.PlayerConnection").getMethod(Version.isCurrentEqualOrHigher(Version.v1_18_R1) ? "a" : "sendPacket",
-		    net.minecraft.network.protocol.Packet.class);
-		constructor = packetType.getConstructor(nmsIChatBaseComponent, int.class);
-		getHandle = Class.forName("org.bukkit.craftbukkit." + Version.getCurrent() + ".entity.CraftPlayer").getMethod("getHandle");
+        if (Version.isCurrentEqualOrHigher(Version.v1_19_R1)) {
+            try {
+                packetType = net.minecraft.network.protocol.game.ClientboundSystemChatPacket.class;
+                nmsIChatBaseComponent = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
+                nmsChatSerializer = Class.forName("net.minecraft.network.chat.IChatBaseComponent$ChatSerializer");
+                playerConnection = Class.forName("net.minecraft.server.level.EntityPlayer").getField("b");
+                sendPacket = Class.forName("net.minecraft.server.network.PlayerConnection").getMethod(Version.isCurrentEqualOrHigher(Version.v1_18_R1) ? "a" : "sendPacket",
+                    net.minecraft.network.protocol.Packet.class);
+                getHandle = Class.forName("org.bukkit.craftbukkit." + Version.getCurrent() + ".entity.CraftPlayer").getMethod("getHandle");
 
-	    } catch (Throwable e) {
-		e.printStackTrace();
-	    }
-	} else if (Version.isCurrentEqualOrHigher(Version.v1_17_R1)) {
-	    try {
-		packetType = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutChat");
-		nmsChatSerializer = Class.forName("net.minecraft.network.chat.IChatBaseComponent$ChatSerializer");
-		nmsIChatBaseComponent = net.minecraft.network.chat.IChatBaseComponent.class;
-		getHandle = Class.forName("org.bukkit.craftbukkit." + Version.getCurrent() + ".entity.CraftPlayer").getMethod("getHandle");
-		playerConnection = net.minecraft.server.level.EntityPlayer.class.getField("b");
-		sendPacket = net.minecraft.server.network.PlayerConnection.class.getMethod(Version.isCurrentEqualOrHigher(Version.v1_18_R1) ? "a" : "sendPacket",
-		    net.minecraft.network.protocol.Packet.class);
-		ChatMessageclz = net.minecraft.network.chat.ChatMessageType.class;
+                if (Version.isCurrentSubEqual(0))
+                    constructor = packetType.getConstructor(nmsIChatBaseComponent, int.class);
+                else
+                    constructor = packetType.getConstructor(nmsIChatBaseComponent, boolean.class);
 
-		consts = ChatMessageclz.getEnumConstants();
-		sub = consts[2].getClass();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        } else if (Version.isCurrentEqualOrHigher(Version.v1_17_R1)) {
+            try {
+                packetType = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutChat");
+                nmsChatSerializer = Class.forName("net.minecraft.network.chat.IChatBaseComponent$ChatSerializer");
+                nmsIChatBaseComponent = net.minecraft.network.chat.IChatBaseComponent.class;
+                getHandle = Class.forName("org.bukkit.craftbukkit." + Version.getCurrent() + ".entity.CraftPlayer").getMethod("getHandle");
+                playerConnection = net.minecraft.server.level.EntityPlayer.class.getField("b");
+                sendPacket = net.minecraft.server.network.PlayerConnection.class.getMethod(Version.isCurrentEqualOrHigher(Version.v1_18_R1) ? "a" : "sendPacket",
+                    net.minecraft.network.protocol.Packet.class);
+                ChatMessageclz = net.minecraft.network.chat.ChatMessageType.class;
 
-	    } catch (Throwable e) {
-		e.printStackTrace();
-	    }
-	} else if (Version.isCurrentHigher(Version.v1_7_R4)) {
-	    Version version = Version.getCurrent();
-	    try {
-		packetType = Class.forName("net.minecraft.server." + version + ".PacketPlayOutChat");
-		Class<?> typeCraftPlayer = Class.forName("org.bukkit.craftbukkit." + version + ".entity.CraftPlayer");
-		Class<?> typeNMSPlayer = Class.forName("net.minecraft.server." + version + ".EntityPlayer");
-		Class<?> typePlayerConnection = Class.forName("net.minecraft.server." + version + ".PlayerConnection");
-		nmsChatSerializer = Class.forName(getChatSerializerClasspath());
-		nmsIChatBaseComponent = Class.forName("net.minecraft.server." + version + ".IChatBaseComponent");
-		getHandle = typeCraftPlayer.getMethod("getHandle");
-		playerConnection = typeNMSPlayer.getField("playerConnection");
-		sendPacket = typePlayerConnection.getMethod("sendPacket", Class.forName("net.minecraft.server." + version + ".Packet"));
-		if (Version.isCurrentHigher(Version.v1_11_R1)) {
-		    ChatMessageclz = Class.forName("net.minecraft.server." + version + ".ChatMessageType");
-		    consts = ChatMessageclz.getEnumConstants();
-		    sub = consts[2].getClass();
-		}
-	    } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | NoSuchFieldException ex) {
-		CMIMessages.consoleMessage("Error {0} ");
-		CMIMessages.consoleMessage(ex.toString());
-	    }
-	}
+                consts = ChatMessageclz.getEnumConstants();
+                sub = consts[2].getClass();
+
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        } else if (Version.isCurrentHigher(Version.v1_7_R4)) {
+            Version version = Version.getCurrent();
+            try {
+                packetType = Class.forName("net.minecraft.server." + version + ".PacketPlayOutChat");
+                Class<?> typeCraftPlayer = Class.forName("org.bukkit.craftbukkit." + version + ".entity.CraftPlayer");
+                Class<?> typeNMSPlayer = Class.forName("net.minecraft.server." + version + ".EntityPlayer");
+                Class<?> typePlayerConnection = Class.forName("net.minecraft.server." + version + ".PlayerConnection");
+                nmsChatSerializer = Class.forName(getChatSerializerClasspath());
+                nmsIChatBaseComponent = Class.forName("net.minecraft.server." + version + ".IChatBaseComponent");
+                getHandle = typeCraftPlayer.getMethod("getHandle");
+                playerConnection = typeNMSPlayer.getField("playerConnection");
+                sendPacket = typePlayerConnection.getMethod("sendPacket", Class.forName("net.minecraft.server." + version + ".Packet"));
+                if (Version.isCurrentHigher(Version.v1_11_R1)) {
+                    ChatMessageclz = Class.forName("net.minecraft.server." + version + ".ChatMessageType");
+                    consts = ChatMessageclz.getEnumConstants();
+                    sub = consts[2].getClass();
+                }
+            } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | NoSuchFieldException ex) {
+                CMIMessages.consoleMessage("Error {0} ");
+                CMIMessages.consoleMessage(ex.toString());
+            }
+        }
     }
 
     public static void send(CommandSender receivingPacket, String msg) {
-	if (receivingPacket instanceof Player)
-	    send((Player) receivingPacket, msg);
-	else
-	    receivingPacket.sendMessage(msg);
+        if (receivingPacket instanceof Player)
+            send((Player) receivingPacket, msg);
+        else
+            receivingPacket.sendMessage(msg);
     }
 
     public static void send(CMICommandSender receivingPacket, String msg) {
-	send(receivingPacket, msg, 0);
+        send(receivingPacket, msg, 0);
     }
 
     public static void send(CMICommandSender receivingPacket, String msg, int keepFor) {
-	if (receivingPacket.isPlayer())
-	    send(receivingPacket.getPlayer(), msg, keepFor);
-	else
-	    receivingPacket.sendMessage(msg);
+        if (receivingPacket.isPlayer())
+            send(receivingPacket.getPlayer(), msg, keepFor);
+        else
+            receivingPacket.sendMessage(msg);
     }
 
     static HashMap<UUID, repeatingActionBar> actionbarMap = new HashMap<UUID, repeatingActionBar>();
 
     public static void send(Player receivingPacket, String msg) {
-	send(receivingPacket, msg, 0);
+        send(receivingPacket, msg, 0);
     }
 
     public static void send(Player receivingPacket, String msg, int keepFor) {
-	send(Arrays.asList(receivingPacket), msg, keepFor);
+        send(Arrays.asList(receivingPacket), msg, keepFor);
     }
 
     public static void send(List<Player> receivingPacket, String msg) {
-	send(receivingPacket, msg, 0);
+        send(receivingPacket, msg, 0);
     }
 
     @SuppressWarnings("deprecation")
     public static void send(List<Player> receivingPacket, String msg, int keepFor) {
 
-	if (receivingPacket == null)
-	    return;
-	if (msg == null)
-	    return;
+        if (receivingPacket == null)
+            return;
+        if (msg == null)
+            return;
 
-	keepFor--;
-	keepFor = keepFor < 0 ? 0 : keepFor;
-	try {
-	    if (!Version.getCurrent().isHigher(Version.v1_7_R4) || nmsChatSerializer == null) {
-		msg = CMIChatColor.translate(msg);
-		for (Player player : receivingPacket) {
-		    if (player == null)
-			continue;
-		    if (!player.isOnline())
-			continue;
-		    player.sendMessage(msg);
-		}
-		return;
-	    }
+        keepFor--;
+        keepFor = keepFor < 0 ? 0 : keepFor;
+        try {
+            if (!Version.getCurrent().isHigher(Version.v1_7_R4) || nmsChatSerializer == null) {
+                msg = CMIChatColor.translate(msg);
+                for (Player player : receivingPacket) {
+                    if (player == null)
+                        continue;
+                    if (!player.isOnline())
+                        continue;
+                    player.sendMessage(msg);
+                }
+                return;
+            }
 
-	    if (constructor == null) {
-		if (Version.isCurrentEqualOrHigher(Version.v1_19_R1)) {
-		    constructor = packetType.getConstructor(nmsIChatBaseComponent, int.class);
-		} else if (Version.isCurrentHigher(Version.v1_15_R1)) {
-		    constructor = packetType.getConstructor(nmsIChatBaseComponent, sub, UUID.class);
-		} else if (Version.isCurrentHigher(Version.v1_11_R1)) {
-		    constructor = packetType.getConstructor(nmsIChatBaseComponent, sub);
-		} else if (Version.isCurrentHigher(Version.v1_7_R4)) {
-		    constructor = packetType.getConstructor(nmsIChatBaseComponent, byte.class);
-		} else {
-		    constructor = packetType.getConstructor(nmsIChatBaseComponent, int.class);
-		}
-	    }
+            RawMessage rm = new RawMessage();
+            rm.addText(msg);
+            Object serialized = nmsChatSerializer.getMethod("a", String.class).invoke(null, CMIChatColor.translate(rm.getRaw()));
 
-	    RawMessage rm = new RawMessage();
-	    rm.addText(msg);
-	    Object serialized = nmsChatSerializer.getMethod("a", String.class).invoke(null, CMIChatColor.translate(rm.getRaw()));
+            Object p = null;
 
-	    Object p = null;
+            for (Player player : receivingPacket) {
+                if (player == null)
+                    continue;
 
-	    for (Player player : receivingPacket) {
-		if (player == null)
-		    continue;
+                if (Version.isCurrentEqualOrHigher(Version.v1_19_R1)) {
+                    if (Version.isCurrentSubEqual(0))
+                        p = constructor.newInstance(serialized, 2);
+                    else
+                        p = constructor.newInstance(serialized, true);
+                } else if (Version.isCurrentHigher(Version.v1_15_R1)) {
+                    p = constructor.newInstance(serialized, consts[2], new UUID(0, 0));
+                } else if (Version.isCurrentHigher(Version.v1_11_R1)) {
+                    p = constructor.newInstance(serialized, consts[2]);
+                } else if (Version.isCurrentHigher(Version.v1_7_R4)) {
+                    p = constructor.newInstance(serialized, (byte) 2);
+                } else {
+                    p = constructor.newInstance(serialized, 2);
+                }
+                Object cplayer = getHandle.invoke(player);
+                Object connection = playerConnection.get(cplayer);
 
-		if (Version.isCurrentEqualOrHigher(Version.v1_19_R1)) {
-		    p = constructor.newInstance(serialized, 2);
-		} else if (Version.isCurrentHigher(Version.v1_15_R1)) {
-		    p = constructor.newInstance(serialized, consts[2], new UUID(0, 0));
-		} else if (Version.isCurrentHigher(Version.v1_11_R1)) {
-		    p = constructor.newInstance(serialized, consts[2]);
-		} else if (Version.isCurrentHigher(Version.v1_7_R4)) {
-		    p = constructor.newInstance(serialized, (byte) 2);
-		} else {
-		    p = constructor.newInstance(serialized, 2);
-		}
-		Object cplayer = getHandle.invoke(player);
-		Object connection = playerConnection.get(cplayer);
+                Object packet = p;
 
-		Object packet = p;
+                if (keepFor < 1) {
+                    Bukkit.getScheduler().runTaskAsynchronously(CMILib.getInstance(), () -> {
+                        try {
+                            sendPacket.invoke(connection, packet);
+                        } catch (Throwable e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    return;
+                }
 
-		if (keepFor < 1) {
-		    Bukkit.getScheduler().runTaskAsynchronously(CMILib.getInstance(), () -> {
-			try {
-			    sendPacket.invoke(connection, packet);
-			} catch (Throwable e) {
-			    e.printStackTrace();
-			}
-		    });
-		    return;
-		}
+                repeatingActionBar old = actionbarMap.remove(player.getUniqueId());
+                if (old != null)
+                    old.cancel();
+                old = new repeatingActionBar();
+                old.setUntil(System.currentTimeMillis() + (keepFor * 1000L));
 
-		repeatingActionBar old = actionbarMap.remove(player.getUniqueId());
-		if (old != null)
-		    old.cancel();
-		old = new repeatingActionBar();
-		old.setUntil(System.currentTimeMillis() + (keepFor * 1000L));
+                actionbarMap.put(player.getUniqueId(), old);
 
-		actionbarMap.put(player.getUniqueId(), old);
+                old.setId(Bukkit.getScheduler().scheduleAsyncRepeatingTask(CMILib.getInstance(), new Runnable() {
+                    @Override
+                    public void run() {
+                        repeatingActionBar old = actionbarMap.get(player.getUniqueId());
+                        if (old == null)
+                            return;
 
-		old.setId(Bukkit.getScheduler().scheduleAsyncRepeatingTask(CMILib.getInstance(), new Runnable() {
-		    @Override
-		    public void run() {
-			repeatingActionBar old = actionbarMap.get(player.getUniqueId());
-			if (old == null)
-			    return;
+                        if (old.getUntil() < System.currentTimeMillis()) {
+                            old.cancel();
+                            return;
+                        }
 
-			if (old.getUntil() < System.currentTimeMillis()) {
-			    old.cancel();
-			    return;
-			}
+                        try {
+                            sendPacket.invoke(connection, packet);
+                        } catch (Throwable e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, 0L, 20L));
 
-			try {
-			    sendPacket.invoke(connection, packet);
-			} catch (Throwable e) {
-			    e.printStackTrace();
-			}
-		    }
-		}, 0L, 20L));
+            }
 
-	    }
-
-	} catch (Exception ex) {
-	    ex.printStackTrace();
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     private static String getChatSerializerClasspath() {
-	if (!Version.isCurrentHigher(Version.v1_8_R2))
-	    return "net.minecraft.server." + Version.getCurrent() + ".ChatSerializer";
-	return "net.minecraft.server." + Version.getCurrent() + ".IChatBaseComponent$ChatSerializer";// 1_8_R2 moved to IChatBaseComponent
+        if (!Version.isCurrentHigher(Version.v1_8_R2))
+            return "net.minecraft.server." + Version.getCurrent() + ".ChatSerializer";
+        return "net.minecraft.server." + Version.getCurrent() + ".IChatBaseComponent$ChatSerializer";// 1_8_R2 moved to IChatBaseComponent
     }
 }
