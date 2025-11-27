87a424fdb249d26024037e290d8388a690821289
Zrips <zripas@gmail.com>
2025-11-27 14:16:14 +0200

    Biome name fix

--- src/main/java/net/Zrips/CMILib/Container/CMIBiome.java
+++ src/main/java/net/Zrips/CMILib/Container/CMIBiome.java
@@ -6,78 +6,101 @@ import java.util.HashMap;
 import java.util.Set;
 
 import net.Zrips.CMILib.CMILib;
-import net.Zrips.CMILib.Logs.CMIDebug;
 import net.Zrips.CMILib.Messages.CMIMessages;
+import net.Zrips.CMILib.Version.Version;
 
 public class CMIBiome {
 
-    private CMIBiomeType type;
-    private String name;
-    private String localized;
+	private CMIBiomeType type;
+	private String name;
+	private String localized;
 
-    static HashMap<String, CMIBiome> cmiBiomeMap = new HashMap<>();
+	static HashMap<String, CMIBiome> cmiBiomeMap = new HashMap<>();
 
-    static {
+	private static Method kmet = null;
+	private static Method met = null;
 
-        try {
-            Class<?> c = Class.forName("org.bukkit.block.Biome");
+	private static String getBiomeName(Object biome) {
+		if (!Version.isPaperBranch()) {
+			return biome.toString();
+		}
 
-            Object[] biomes = (Object[]) c.getMethod("values").invoke(c);
+		try {
+			if (met == null) {
+				kmet = biome.getClass().getMethod("getKey");
+				Object key = kmet.invoke(biome);
+				met = key.getClass().getMethod("getKey");
+			}
+			return (String) met.invoke(kmet.invoke(biome));
+		} catch (Exception e) {
+		}
+		return biome.toString();
+	}
 
-            for (Object biome : biomes) {
-                if (biome == null)
-                    continue;
-                String name = biome.toString();
-                try {
-                    CMIBiome cbiome = new CMIBiome(name, CMILib.getInstance().getConfigManager().getLocaleConfig().get("info.Biomes." + name, CMIText.firstToUpperCase(name)));
-                    cbiome.type = CMIBiomeType.get(name);
-                    cmiBiomeMap.put(name.toLowerCase().replace("_", "").replace(" ", ""), cbiome);
-                } catch (Exception e) {
-                    CMIMessages.consoleMessage("&4Failed to recognize biome by (" + name + ") name. Skipping.");
-                }
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+	static {
 
-    }
+		try {
+			Class<?> c = Class.forName("org.bukkit.block.Biome");
 
-    CMIBiome(String name, String localized) {
-        this.name = name;
-        this.localized = localized;
-    }
+			Object[] biomes = (Object[]) c.getMethod("values").invoke(c);
 
-    public static CMIBiome get(String name) {
-        return cmiBiomeMap.get(name.toLowerCase().replace("_", ""));
-    }
+			for (Object biome : biomes) {
+				if (biome == null)
+					continue;
 
-    public static String getName(Object biome) {
-        CMIBiome b = cmiBiomeMap.get(biome.toString().toLowerCase().replace("_", ""));
-        return b == null ? CMIText.firstToUpperCase(biome.toString()) : b.getName();
-    }
+				String name = getBiomeName(biome);
 
-    public Color getColor() {
-        if (type == null)
-            return new Color(0, 0, 0);
-        return type.getColor();
-    }
+				try {
+					CMIBiome cbiome = new CMIBiome(name, CMILib.getInstance().getConfigManager().getLocaleConfig().get("info.Biomes." + name, CMIText.firstToUpperCase(name)));
+					cbiome.type = CMIBiomeType.get(name);
+					cmiBiomeMap.put(name.toLowerCase().replace("_", "").replace(" ", ""), cbiome);
+				} catch (Exception e) {
+					CMIMessages.consoleMessage("&4Failed to recognize biome by (" + name + ") name. Skipping.");
+				}
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 
-    public int getId() {
-        if (type == null)
-            return 0;
-        return type.getId();
-    }
+	}
 
-    public String getLocalized() {
-        return localized;
-    }
+	CMIBiome(String name, String localized) {
+		this.name = name;
+		this.localized = localized;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public static CMIBiome get(String name) {
+		return cmiBiomeMap.get(name.toLowerCase().replace("_", ""));
+	}
 
-    public static Set<String> values() {
-        return cmiBiomeMap.keySet();
-    }
+	public static String getName(Object biome) {
+		String name = getBiomeName(biome);
+		CMIBiome b = cmiBiomeMap.get(name.replace("_", ""));
+		return b == null ? CMIText.firstToUpperCase(name) : b.getName();
+	}
+
+	public Color getColor() {
+		if (type == null)
+			return new Color(0, 0, 0);
+		return type.getColor();
+	}
+
+	public int getId() {
+		if (type == null)
+			return 0;
+		return type.getId();
+	}
+
+	public String getLocalized() {
+		return localized;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static Set<String> values() {
+		return cmiBiomeMap.keySet();
+	}
 
 }
